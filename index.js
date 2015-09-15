7bffe6e4be74c728b5cb4e2c7ffac85b0cf9a0f1
Bob Matcuk <bmatcuk@gmail.com>
2015-09-14 21:46:53 -1000

    added options

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var MarkedBrunchStatic, marked;
+var MarkedBrunchStatic, _, marked;
 
 marked = require('marked');
 
+_ = {
+  merge: require('lodash.merge')
+};
+
 MarkedBrunchStatic = (function() {
-  function MarkedBrunchStatic() {}
+  function MarkedBrunchStatic(config1) {
+    var ref;
+    this.config = config1;
+    if ((ref = this.config) != null ? ref.fileMatch : void 0) {
+      this.handles = this.config.fileMatch;
+      delete this.config.fileMatch;
+    }
+  }
 
   MarkedBrunchStatic.prototype.handles = /\.static\.(?:markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)$/;
 
@@ -11,8 +22,10 @@ MarkedBrunchStatic = (function() {
     return filename.replace(/\.static\.\w+$/, '.html');
   };
 
-  MarkedBrunchStatic.prototype.compile = function(data, filename, callback) {
-    return marked(data, function(err, content) {
+  MarkedBrunchStatic.prototype.compile = function(data, filename, options, callback) {
+    var opts;
+    opts = _.merge({}, this.config, options != null ? options.markdown : void 0);
+    return marked(data, opts, function(err, content) {
       if (err) {
         callback(err);
         return;
--- src/marked-brunch-static.coffee
+++ src/marked-brunch-static.coffee
@@ -1,15 +1,21 @@
 marked = require 'marked'
+_ =
+  merge: require 'lodash.merge'
 
 class MarkedBrunchStatic
   constructor: (@config) ->
+    if @config?.fileMatch
+      @handles = @config.fileMatch
+      delete @config.fileMatch
 
   handles: /\.static\.(?:markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)$/
 
   transformPath: (filename) ->
     filename.replace /\.static\.\w+$/, '.html'
 
-  compile: (data, filename, callback) ->
-    marked data, @config, (err, content) ->
+  compile: (data, filename, options, callback) ->
+    opts = _.merge {}, @config, options?.markdown
+    marked data, opts, (err, content) ->
       if err
         callback err
         return
